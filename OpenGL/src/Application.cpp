af87f293cdbdd23028361372a7c279d274a2b634
Patryk Węgrzyn <wegpat@gmail.com>
2019-06-22 19:16:09 +0200

    Add and init dependencies

---
+++ OpenGL/src/Application.cpp
@@ -0,0 +1,40 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Error! " << std::endl;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenGL Basic Rendering
+Basic rendering in OpenGL (using GLFW for cross-platform windows and context managment and GLEW for loading/wrangling the GPU's dll implementation of the modern OpenGL specification)
+
+## Prerequisites
+* Visual Studio 2017+
+
+## Starting
+Just import the project through VS
+
+## Current progress
+* Project Setup (GLEW/GLFW)
